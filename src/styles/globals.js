f1be41a66ab702a8c8c221bb278840d5c7e9150b
erikalopes <201810099@aluno.fjn.edu.br>
2020-05-10 17:01:26 -0300

    concluded icons

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
-import background from '../assets/images/background.svg'
+import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
 

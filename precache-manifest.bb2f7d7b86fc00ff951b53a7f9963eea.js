6d79bffb31ca28d3daacdffc126e5a5499128790
erikalopes <201810099@aluno.fjn.edu.br>
2020-07-27 14:55:42 -0300

    Updates

---
+++ precache-manifest.bb2f7d7b86fc00ff951b53a7f9963eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0078e870796b9e7800a7c6ed3b5fef30",
+    "url": "/rocket-shoes/index.html"
+  },
+  {
+    "revision": "4c1b885e96a404d0bb46",
+    "url": "/rocket-shoes/static/js/2.93f4d91e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rocket-shoes/static/js/2.93f4d91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d43a36b9b8951b2a5c",
+    "url": "/rocket-shoes/static/js/main.8d6e93ac.chunk.js"
+  },
+  {
+    "revision": "4cfd46fd44887100d0d3",
+    "url": "/rocket-shoes/static/js/runtime-main.5e44d1a9.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/rocket-shoes/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/rocket-shoes/static/media/logo.879557c5.svg"
+  }
+]);
